abbed26d5f4e70d8e874aa3fcbeaeccb74c4bde5
JuanMVicente <j_m_vicente@yahoo.com.ar>
2022-07-05 15:35:19 -0300

    Ejercicio 5

---
+++ OpenBootcamp/E05/E05.py
@@ -0,0 +1,11 @@
+nombre = "Juan"
+apellido = "Vicente"
+edad = 36
+email = "j_m_vicente@yahoo.com.ar"
+telefono = "1133367360"
+casado = False
+conHijos = False
+listaDeAmigos = ["Luis","Martin","Federico","Mariano","Juan Esteban","Alvaro"]
+peliculasVistas = {1:"El Señor de los Anillos", 2:"Scary Movie", 3:"Top Secret"}
+
+print(f'{nombre} {apellido} - Edad: {edad} - mail: {email} - telefono: {telefono}\nCasado: {casado} - Hijos: {conHijos}\nAmigos: {listaDeAmigos}\nPeliculas Vistas: {peliculasVistas}')
